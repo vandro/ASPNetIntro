478fb94d28f02329e52e4442a7c0c2e9d4ecf51d
GeekTrainer <chrhar@microsoft.com>
2014-06-20 11:42:37 -0700

    Initial

---
+++ Again/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Again.Startup))]
+namespace Again
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
